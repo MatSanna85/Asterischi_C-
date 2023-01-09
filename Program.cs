afa9549a385fc2a15577328351150d610f767af9
MatSanna85 <108819009+MatSanna85@users.noreply.github.com>
2023-01-09 17:33:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int righe;
+
+        while (true)
+        {
+            Console.WriteLine("Scrivi un numero (altrimenti 'esc' per uscire): ");
+            string s = Console.ReadLine();
+
+            if (s == "esc")
+            {
+                Console.WriteLine("Arrivederci");
+                break;
+            }
+
+            righe = Convert.ToInt32(s);
+            for (int r = 1; r <= righe; r++)
+            {
+                for (int ast = 1; ast <= r; ast++)
+                {
+                    Console.Write("* ");
+                    
+                }
+                Console.Write("\n");
+
+            }
+           
+
+        }
+
+    }
+}
